88c2d473244b4cd8ad6c2a23dc13edbc7ef31bbc
Andrew <a.b.kraev@gmail.com>
2023-01-11 21:24:40 +0300

    Версия на api погода для сайта

--- weather.py
+++ weather.py
@@ -25,7 +25,7 @@ def show_ibtn(i_city):
 # Запрос к yandex погода
 def get_yandex(lat, lon):
     res = requests.get(
-        'https://api.weather.yandex.ru/v2/forecast',
+        'https://api.weather.yandex.ru/v2/informers',
         params={'lat':lat,'lon':lon,'lang':'ru_RU'},
         headers={'X-Yandex-API-Key':'e3c8dd6f-0234-4527-a29c-6fb8b1fa31df'},
     )
@@ -45,39 +45,36 @@ def weather_now(name_city):
         res = get_yandex(config.city.get(name_city)[1],config.city.get(name_city)[2])
     
         temp0 = 'Сейчас в '+ config.city.get(name_city)[0]
-        temp2 = '\nТемпература \t'+str(res['fact']['temp'])+' (°C)'
+        temp2 = 'Температура \t'+str(res['fact']['temp'])+' (°C)'
         temp3 = 'Ощущается как \t'+str(res['fact']['feels_like'])+' (°C)'
         temp4 = 'Скорость ветра \t'+ str(res['fact']['wind_speed'])+' (м/с)'
         temp5 = 'Порывы \t'+ str(res['fact']['wind_gust'])+' (м/с)'         
         temp6 = 'Ветер дует с '+ config.dict_wind_dir.get(res['fact']['wind_dir'])
         temp7 = 'Давление \t'+ str(res['fact']['pressure_mm'])+' в мм рт.ст.'
         temp8 = config.dict_cond.get(res['fact']['condition'])
-        temp9 = res['fact']['prec_type']
+ #       temp9 = res['fact']['prec_type']
         temp = temp0+'\n'+temp2+'\n'+temp3+'\n'+temp4+'\n'+temp5+'\n'+temp6+'\n'+temp7+'\n'+temp8
         
         return temp
         
 def weather_3d(name_city_3d):
         tmp_out = []
-        tmp = ''
-        day = 0
-                
+        tmp = ''              
         res_3d = get_yandex(config.city.get(name_city_3d)[1],config.city.get(name_city_3d)[2])
          
-        for data_3d in res_3d['forecasts']:
-            if day == 0: 
-                tmp = 'Cегодня в '+ config.city.get(name_city_3d)[0]    
-            elif day == 1:
-                tmp = 'Завтра в '+ config.city.get(name_city_3d)[0]
-            elif day == 2:
-                tmp = 'Послезавтра в '+ config.city.get(name_city_3d)[0]
+        for data_3d in res_3d['forecast']['parts']:
+            if data_3d['part_name'] == 'day': 
+                tmp = 'Днем в '+ config.city.get(name_city_3d)[0]+'\nТемпература  '+str(data_3d['temp_avg'])+'(°C)\n'+'Давление '+str(data_3d['pressure_mm'])+' мм рт.ст.\n'+config.dict_cond.get(data_3d['condition']) 
+            elif data_3d['part_name'] == 'night':
+                tmp = 'Ночью в '+ config.city.get(name_city_3d)[0]+'\nТемпература  '+str(data_3d['temp_avg'])+'(°C)\n'+'Давление '+str(data_3d['pressure_mm'])+' мм рт.ст.\n'+config.dict_cond.get(data_3d['condition'])
+            elif data_3d['part_name'] == 'morning':
+                tmp = 'Утром в '+ config.city.get(name_city_3d)[0]+'\nТемпература  '+str(data_3d['temp_avg'])+'(°C)\n'+'Давление '+str(data_3d['pressure_mm'])+' мм рт.ст.\n'+config.dict_cond.get(data_3d['condition'])
+            elif data_3d['part_name'] == 'evening':
+                tmp = 'Вечером в '+ config.city.get(name_city_3d)[0]+'\nТемпература '++str(data_3d['temp_avg'])+'(°C)\n'+'Давление '+str(data_3d['pressure_mm'])+' мм рт.ст.\n'+config.dict_cond.get(data_3d['condition']) 
             else:
-                break
-#            day_out = datetime.datetime.strptime(data_3d['date'],'%Y-%m-%d')
-#             day_out.strftime('%d.%m.%Y')
-            tmp = tmp +'\n'+'\nТемпература  {:>3} {:>4}\nДавление  {:>3} {}\n{}'.format(str(data_3d['parts']['day_short']['temp']),'(°C)',data_3d['parts']['day_short']['pressure_mm'],'мм рт.ст.',config.dict_cond.get(data_3d['parts']['day_short']['condition']))
+                tmp = 'op'
+
             tmp_out.append(tmp)
-            day = day +1
        
         return tmp_out
 
@@ -106,14 +103,16 @@ def callback(call):
     if call.message:
         if call.data == 'Кумены':
             bot.answer_callback_query(call.id)
-            bot.send_message(call.message.chat.id, weather_3d(call.data)[0])
-            bot.send_message(call.message.chat.id, weather_3d(call.data)[1])
-            bot.send_message(call.message.chat.id, weather_3d(call.data)[2])
+            tt = weather_3d(call.data)
+            bot.send_message(call.message.chat.id, tt[0])
+            bot.send_message(call.message.chat.id, tt[1])
+#            bot.send_message(call.message.chat.id, weather_3d(call.data)[2])
         elif call.data == 'Киров':
             bot.answer_callback_query(call.id)
-            bot.send_message(call.message.chat.id, weather_3d(call.data)[0])
-            bot.send_message(call.message.chat.id, weather_3d(call.data)[1])
-            bot.send_message(call.message.chat.id, weather_3d(call.data)[2])
+            ttk = weather_3d(call.data)
+            bot.send_message(call.message.chat.id, ttk[0])
+            bot.send_message(call.message.chat.id, ttk[1])
+   #         bot.send_message(call.message.chat.id, weather_3d(call.data)[2])
         else:
             bot.answer_callback_query(call.id)
             bot.send_message(call.message.chat.id, 'Тут пока ничего нет')        
